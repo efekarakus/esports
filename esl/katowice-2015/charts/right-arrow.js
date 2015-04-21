60d7c07a7d07df1a490f376139c80a8b2d4fe1b2
Efe Karakus <efekarakus@gmail.com>
2015-04-21 14:25:39 -0500

    change charts as arrows are pressed

---
+++ esl/katowice-2015/charts/right-arrow.js
@@ -0,0 +1,100 @@
+function rightArrow() {
+	var width = 100,
+		height = 300;
+
+	var sideLength = width/2;
+	var svg;
+
+	var titles = [],
+		titleIndex = 1;
+
+	function chart(selection) {
+		selection.each(function(data) {
+			titles = data.titles;
+
+			svg = d3.select(this)
+				.append("svg")
+					.attr("width", width)
+					.attr("height", height);
+
+			var arrow = svg.append("g")
+					.attr("class", "visible");
+
+			var triangleHeight = Math.sqrt(sideLength*sideLength - (sideLength/2)*(sideLength/2));
+
+			var positions = [
+				{x: (width/2 - sideLength/2), y: (height/2 - sideLength/2)},
+				{x: (width/2 - sideLength/2), y: (height/2 + sideLength/2)},
+				{x: (width/2 - sideLength/2) + triangleHeight, y: height/2}
+			]
+
+			arrow.append("path")
+				.attr("d",	"M " + positions[0].x + " " + positions[0].y + " " +
+							"L " + positions[1].x + " " + positions[1].y + " " +
+							"L " + positions[2].x + " " + positions[2].y + " " +
+							"L " + positions[0].x + " " + positions[0].y);
+
+		})
+	}
+
+
+	chart.width = function(_) {
+		if (!arguments.length) return width;
+		width = _;
+		return chart;
+	}
+
+	chart.height = function(_) {
+		if (!arguments.length) return height;
+		height = _;
+		return chart;
+	}
+
+	chart.sideLength = function(_) {
+		if (!arguments.length) return sideLength;
+		sideLength = _;
+		return chart;
+	}
+
+	chart.turnInvisible = function() {
+		svg.select("g.visible")
+				.attr("class", "invisible");
+	}
+
+	chart.turnVisible = function() {
+		svg.select("g.invisible")
+				.attr("class", "visible");
+	}
+
+	chart.prev = function() {
+		if (titleIndex === titles.length) {
+			chart.turnVisible();
+		}
+
+		if (titleIndex !== -1) {
+			titleIndex -= 1;
+			// TODO update tooltip
+		}
+	}
+
+	chart.next = function() {
+		if (titleIndex === titles.length || titleIndex === (titles.length - 1)) {
+			titleIndex = titles.length;
+			chart.turnInvisible();
+		} else {
+			titleIndex += 1;
+			// TODO update tooltip
+		}
+	}
+
+	chart.onClick = function(f) {
+		var arrow = svg.select("g");
+
+		arrow.on("click", function() {
+			f();
+			chart.next();
+		});
+	}
+
+	return chart;
+}
--- esl/katowice-2015/js/esl.js
+++ esl/katowice-2015/js/esl.js
@@ -4,24 +4,68 @@ $(document).ready(function() {
 
 		if (error) return console.warn(error);
 
+		// draw the segmented area chart
 		var streams = data.streams.reverse();
-
-		var streamId = streams[0].stream_id;
-
+		var selectedIdx = 0;
 		var chart = segmentedAreaChart();
+		var parsedIndices = {0: true, 1: false, 2: false, 3: false}
 
 		d3.select("#esl-one-chart")
-			.datum(streams[0])
+			.datum(streams[selectedIdx])
 			.call(chart);
 
+		// draw the arrows
+		var previousArrow = leftArrow();
+		d3.select(".left.arrow")
+			.datum({
+				titles: streams.filter(function(s) { return s.day; })
+			})
+			.call(previousArrow);
+		previousArrow.turnInvisible();
 
+		var nextArrow = rightArrow();
+		d3.select(".right.arrow")
+			.datum({
+				titles: streams.filter(function(s) { return s.day; })
+			})
+			.call(nextArrow);
+
+		// click functions for arrows
+
+		previousArrow.onClick(function() {
+			nextArrow.prev();
+			selectedIdx -= 1;
+
+			if (parsedIndices[selectedIdx]) chart.formatDate(false);
+
+			d3.select("#esl-one-chart").select("svg").remove();
+			
+			d3.select("#esl-one-chart")
+				.datum(streams[selectedIdx])
+				.call(chart);
+		})
+
+		nextArrow.onClick(function() {
+			previousArrow.next();
+			selectedIdx += 1;
+			if (!parsedIndices[selectedIdx]) {
+				chart.formatDate(true);
+				parsedIndices[selectedIdx] = true;
+			}
+			d3.select("#esl-one-chart").select("svg").remove();
+			
+			d3.select("#esl-one-chart")
+				.datum(streams[selectedIdx])
+				.call(chart);
+		});
+
+		// draw the retention chart
 		var retentionChart = segmentedAreaChart()
 								.margin({top: 10, right: 30, bottom: 0, left: 50})
 								.width(280)
 								.height(200)
 								.showTitle(false)
 								.formatDate(false);
-
 		d3.select("#retention-chart")
 			.datum({
 				areas: [streams[0].areas[4]]
