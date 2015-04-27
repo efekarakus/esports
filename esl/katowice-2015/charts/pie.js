b416f0248a1dde3b9e0ead3674a4aced8214aba7
Efe Karakus <efekarakus@gmail.com>
2015-04-27 12:48:09 -0500

    done with pie charts

--- esl/katowice-2015/charts/pie.js
+++ esl/katowice-2015/charts/pie.js
@@ -12,10 +12,16 @@ function pie()
 	function chart(selection) {
 		selection.each(function(data) {
 			
+			var totalSeconds = 0;
 			data.forEach(function(d) {
 				d.total = (+d.hours)*3600 + (+d.minutes)*60 + (+d.seconds);
+				totalSeconds += d.total;
 			});
 
+			data.forEach(function(d) {
+				d.percentage = (d.total/totalSeconds)*100.0;
+				d.percentage = d.type === "analysis" ? Math.floor(d.percentage) : Math.round(d.percentage);
+			});
 
 			var arc = d3.svg.arc()
 				.outerRadius(radius)
@@ -38,6 +44,18 @@ function pie()
 				.attr("d", arc)
 				.attr("class", function(d) { return d.data.type; });
 
+
+			g.append("text")
+				.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+				.attr("dy", ".35em")
+				.style("text-anchor", "middle")
+				.text(function(d) {return d.data.type; });
+
+			g.append("text")
+				.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+				.attr("dy", "1.75em")
+				.style("text-anchor", "middle")
+				.text(function(d) {return d.data.percentage + "%"; });
 		})
 	}
 
